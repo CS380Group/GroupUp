c89a80476b0b5e6b39d987c98c74818bc78f11f7
alexworland <alexworland@gmail.com>
2021-05-09 15:11:06 -0700

    Refactored the connection code to its own script.

--- Project/app/backend/getEventsByCategory.php
+++ Project/app/backend/getEventsByCategory.php
@@ -5,17 +5,8 @@
 
     // TODO: Database connection can be extracted into a single file
 
-    // Database connection information. Will be different once database is on server
-    $dbhost = 'localhost';
-    $dbuser = 'root';
-    $dbpass = '123';
-    $db = 'GroupUp';
-    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-
-    // Checks if the connection to the database is successful
-    if(! $conn ) {
-        die('Could not connect: ' . mysql_error());
-    }
+    // Connect to database
+    include 'connectToDatabase.php';
     
     // Debug message
     // echo "Connected successfully.";
