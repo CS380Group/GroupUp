7bdda0a2b9c25486651198e45d9fa6dd4a66a123
AlexWorland <alexworland@gmail.com>
2021-05-13 15:17:19 -0700

    Altered the scheme of the database

--- backend/addEventToDatabase.php
+++ backend/addEventToDatabase.php
@@ -7,24 +7,48 @@
     // Connect to database
     include 'connectToDatabase.php';
 
+    // Include geocode funtion
+    include 'geocodePosition.php';
+
     // Checks if the submit button has been pressed
     if(isset($_POST['submit'])) {
 
         //We need userName, userTitle, userType, userDescription, userPhone, userAddress, userCity, userState
 
         $userName = $_POST['userName'];
-        $userTitle = $_POST['userTitle'];
-        $userType = $_POST['userType'];
-        $userDescription = $_POST['userDescription'];
+        $eventTitle = $_POST['userTitle'];
+        $eventType = $_POST['userType'];
+        $eventDescription = $_POST['userDescription'];
         $userPhone = $_POST['userPhone'];
         $userPhone = preg_replace('/[^0-9]/', '', $userPhone);
-        $userAddress = $_POST['userAddress'];
-        $userCity = $_POST['userCity'];
-        $userState = $_POST['userState'];
+        $eventAddress = $_POST['userAddress'];
+        $eventCity = $_POST['userCity'];
+        $eventState = $_POST['userState'];
+
+        // Geocode address to get lat/long
+        $eventAddress = $eventAddress . $eventCity . $eventState;
+        $geoCode = geocode($eventAddress);
+        if (!$geoCode) {
+            die("geocode failed");    
+        }
+        print_r($geoCode);
+
+        // Extract location data from geocode
+        $latitude = $geoCode[0];
+        $longitude = $geoCode[1];
+        $location = explode(',', $geoCode[2]);
+        $street = $location[0];
+        $city = $location[1];
+        $city = preg_replace('/[^A-Za-z]/', '', $city);
+        $stateZip = explode(' ', $location[2]);
+        $state = $stateZip[1];
+        $zip = $stateZip[2];
+        $country = $location[3];
+        $country = preg_replace('/[^A-Za-z]/', '', $country); 
 
         // Stores the sql query to be executed
-        $query = "INSERT INTO event (userName, userTitle, userType, userDescription, userPhone, userAddress, userCity, userState) 
-            VALUES ('$userName', '$userTitle', '$userType', '$userDescription', '$userPhone', '$userAddress', '$userCity', '$userState')";
+        $query = "INSERT INTO event (userName, eventTitle, eventType, eventDescription, userPhone, eventStreet, eventCity, eventState, eventZip, eventCountry, latitude, longitude) 
+            VALUES ('$userName', '$eventTitle', '$eventType', '$eventDescription', '$userPhone', '$street', '$city', '$state', '$zip', '$country', '$latitude', '$longitude')";
 
         // Execute the query and inform the user if it was successfull
         if (!mysqli_query($conn, $query)) {
