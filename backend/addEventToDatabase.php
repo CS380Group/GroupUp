51da10aed4d999f1d5c4f13419bce30ed0d5cb2d
AlexWorland <alexworland@gmail.com>
2021-05-23 21:50:07 -0700

    Protected add event feature against SQL injection, added date/time fields to each event

--- backend/addEventToDatabase.php
+++ backend/addEventToDatabase.php
@@ -25,7 +25,7 @@
         $conn = connectToDatabase();
 
         // Get current date
-        $dateOfCreation = date('Y-m-d');
+        $dateOfCreation = date('Ymd H:i:s');
 
         // Prepare event variables
         $userName = $_SESSION['username'];
@@ -42,39 +42,49 @@
         $stmt = $conn->prepare("SELECT userId FROM user WHERE emailAddress=?");
         // Check for errors
         if ($stmt == false) {
+            echo "Something went wrong when preparing the get userId statement. Please try again.";
             displayRedirect();
-            die("Something went wrong when preparing the get userId statement. Please try again.");
+            die();
         }
         
+        // Bind parameters for query
         $stmt->bind_param('s', $userName);
         // Check for errors
         if ($stmt == false) {
+            echo "Something went wrong when binding paramerters of the get userId statement. Please try again.";
             displayRedirect();
-            die("Something went wrong when binding paramerters of the get userId statement. Please try again.");
+            die();
         }
 
+        // Execute the query
         $stmt->execute();
         // Check for errors
         if ($stmt == false) {
+            echo "Something went wrong when executing the get userId statement. Please try again.";
             displayRedirect();
-            die("Something went wrong when executing the get userId statement. Please try again.");
+            die();
         }
 
+        // Get the results of the query
         $result = $stmt->get_result();
         // Check for errors
         if ($stmt == false) {
+            echo "Something went wrong when getting the result from the get userId statement. Please try again.";
             displayRedirect();
-            die("Something went wrong when getting the result from the get userId statement. Please try again.");
+            die();
         }
 
+        // Close the connection
         $stmt->close();
 
+        // If the number of entries is 0, the user does not exist in the database.
         if (mysqli_num_rows($result) == 0) {
             echo "Error: Please make sure you're signed in before submitting an event.";
             displayRedirect();
             die();
         }
 
+        // If the number of users returned is larger than one, something is wrong with the database
         if (mysqli_num_rows($result) > 1) {
             echo "Error: Something went wrong with the database! Duplicate email addresses aren't allowed!";
             displayRedirect();
@@ -84,15 +94,14 @@
         // Get the userId of the email address
         $row = mysqli_fetch_assoc($result);
         $userId = $row['userId'];
-        
-        // Debug
-        // echo "UserId for user $userName is $userId ";
-
+   
         // Geocode address to get lat/long
         $eventAddress = $eventAddress . $eventCity . $eventState;
         $geoCode = geocode($eventAddress);
         if (!$geoCode) {
-            die("geocode failed");    
+            echo "Error: Something went wrong during the geocode.";
+            displayRedirect();
+            die();    
         }
 
         // Extract location data from geocode
@@ -106,23 +115,63 @@
         $state = $stateZip[1];
         $zip = $stateZip[2];
         $country = $location[3];
-        $country = preg_replace('/[^A-Za-z]/', '', $country); 
+        $country = preg_replace('/[^A-Za-z]/', '', $country);
 
-        // Stores the sql query to be executed
-        $query = "INSERT INTO event (userId, eventTitle, eventType, eventDescription, eventPhone, eventStreet, eventCity, eventState, eventZip, eventCountry, latitude, longitude) 
-            VALUES ('$userId', '$eventTitle', '$eventType', '$eventDescription', '$eventPhone', '$street', '$city', '$state', '$zip', '$country', '$latitude', '$longitude')";
+        // Temp variables
+        $startDate = date('Y-m-d H:i:s');
+        $endDate = date('Y-m-d H:i:s'); 
 
-        // Execute the query and inform the user if it was successfull
-        if (!mysqli_query($conn, $query)) {
-            echo('An error occurred when submitting the event!');
-        } else {
-            echo "Event submitted successfully!";
+        // Prepare the insert event statement
+        $stmt = $conn->prepare(
+            "INSERT INTO event (
+                userId, 
+                eventTitle, 
+                eventType, 
+                eventDescription, 
+                eventPhone, 
+                eventStreet, 
+                eventCity, 
+                eventState, 
+                eventZip, 
+                eventCountry, 
+                latitude, 
+                longitude,
+                startDate,
+                endDate
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        if ($stmt == false) {
+            echo "Something went wrong while preparing the query. Please try again.";
+            displayRedirect();
+            die();
         }
         
+        // Bind the parameters for the insert event statement
+        $stmt->bind_param('ssssssssssssss', $userId, $eventTitle, $eventType, $eventDescription, $eventPhone, $street, $city, $state, $zip, $country, $latitude, $longitude, $startDate, $endDate);
+        if ($stmt == false) {
+            echo "Something went wrong while binding parameters to the query. Please try again.";
+            displayRedirect();
+            die();
+        }
+
+        // Execute the insert event statement
+        $stmt->execute();
+        if ($stmt == false) {
+            echo "Something went wrong while executing the query. Please try again.";
+            displayRedirect();
+            die();
+        } else {
+            echo "Event submitted successfully.";
+        }
+
+
+        // Close the connection
+        $stmt->close();
+
         // Close the connection to the database
         mysqli_close($conn);
+    } else {
+        echo "Something went wrong :(";
     }
 
     displayRedirect();
-
 ?>
